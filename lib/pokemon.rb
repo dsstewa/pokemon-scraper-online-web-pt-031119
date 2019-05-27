ee028314896e090f58afe07a1604c5ee67520694
David Stewart <davidstewart@gmail.com>
2019-05-27 19:23:35 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,11 +4,12 @@ class Pokemon
   attr_accessor :name, :type, :db, :hp
   attr_reader :id
   
-  def initialize(name, type=nil, db=nil, id=nil)
+  def initialize(name, type=nil, db=nil, id=nil, hp = nil)
     @name = name
     @type = type
     @db = db
     @id = id
+    @hp = hp
   end
   
   def self.save(name, type, db)
@@ -25,6 +26,7 @@ class Pokemon
     SQL
     #binding.pry
     dood = db.execute(sql,id).last
+    binding.pry
     a = Pokemon.new(dood[1],dood[2],db,dood[0])
     a
     
